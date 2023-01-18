8819f916f4969f14a3dbeaf69faa453888b3880e
Daneri RS <daneri_pili@hotmail.com>
2023-01-17 18:24:56 -0600

    agrego carpetas

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString  ="Pura prueba"
+let b = miString + "nomas pa calar"
+console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "holi"
+
+    console.log(a)
+}
